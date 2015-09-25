7ce66909b1cd315e4e48e2575f9440b833c8ca2c
Eduardo Zulian <eduardozulian@gmail.com>
2015-09-25 12:15:31 -0300

    First commit
    We are using the WordPress Plugin Boilerplate http://wppb.me/

---
+++ includes/class-parlamentar-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://zulian.org
+ * @since      1.0.0
+ *
+ * @package    Parlamentar
+ * @subpackage Parlamentar/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Parlamentar
+ * @subpackage Parlamentar/includes
+ * @author     Eduardo Zulian <ola@zulian.org>
+ */
+class Parlamentar_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-parlamentar-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://zulian.org
+ * @since      1.0.0
+ *
+ * @package    Parlamentar
+ * @subpackage Parlamentar/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Parlamentar
+ * @subpackage Parlamentar/includes
+ * @author     Eduardo Zulian <ola@zulian.org>
+ */
+class Parlamentar_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
