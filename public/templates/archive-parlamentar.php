ad5ceda7b5b346cd5b43e40947a5cf8a7c90b202
Eduardo Zulian <eduardozulian@gmail.com>
2015-09-27 17:18:26 -0300

    Update archive layout

--- public/templates/archive-parlamentar.php
+++ public/templates/archive-parlamentar.php
@@ -25,27 +25,28 @@ get_header(); ?>
 				?>
 			</header><!-- .page-header -->
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+			<div class="parlamentar-list">
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<header class="entry-header">
-						<?php if ( has_post_thumbnail() ) : ?>
-						<div class="entry-image">
-							<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_post_thumbnail( 'parlamentar-archive' ); ?></a>
-						</div><!-- .entry-image -->
-						<?php endif; ?>
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<header class="entry-header">
+							<?php if ( has_post_thumbnail() ) : ?>
+							<div class="entry-image">
+								<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_post_thumbnail( 'parlamentar-archive' ); ?></a>
+							</div><!-- .entry-image -->
+							<?php endif; ?>
 
-						<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-					</header><!-- .entry-header -->
-				</article><!-- #post-## -->
+							<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+						</header><!-- .entry-header -->
+					</article><!-- #post-## -->
 
-			<?php endwhile; ?>
+				<?php endwhile; ?>
+			</div>
 
 		<?php endif; ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
